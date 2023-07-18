7c0ed597e538bf859af5dd59909d9a212ab8ad32
Stuart Woods <stuartwoods@outlook.com>
2023-07-18 10:32:57 +0100

    Update Add Hosts to vCenter.ps1

--- Add Hosts to vCenter.ps1
+++ Add Hosts to vCenter.ps1
@@ -1,31 +1,51 @@
-﻿<# PowerCLI script to add ESXi hosts to vCenter Server
+﻿Start-Transcript -Path "C:\PS\Transcripts\AddHostsToVC.txt"
+
+<# PowerCLI script to add ESXi hosts to vCenter Server
 
 Specify vCenter Server, vCenter Server username, vCenter Server user password, vCenter Server location which can be the Datacenter, a Folder or a Cluster (which I used).
 #>
-$vCenter="192.168.1.100"
+$vCenter="vcenter.lab.local"
 $vCenterUser="administrator@vsphere.local"
-$vCenterUserPassword="Passw0rd123!"
-$vcenterlocation="sw-cluster"
+$vCenterUserPassword="Password123!"
+$vcenterlocation="lab-cluster"
 
 # Specify the ESXi host you want to add to vCenter Server and the user name and password to be used #
-$esxihosts=(192.168.1.10,192.168.1.20)
+$esxihosts=("esxi-01.lab.local","esxi-02.lab.local")
 $esxihostuser="root"
-$esxihostpasswd="Passw0rd123!"
+$esxihostpasswd="password123"
 
+Clear-Host
+write-host
+write-host
+write-host
+write-host
+write-host
+write-host
+write-host
+write-host
 #Connect to vCenter Server
-write-host Connecting to vCenter Server $vcenter -foreground green
+write-host Connecting to $vcenter -foreground Green
 Connect-viserver $vCenter -user $vCenterUser -password $vCenterUserPassword -WarningAction 0 | out-null
 
-write-host --------
-write-host Start adding ESXi hosts to the vCenter Server $vCenter
-write-host --------
+write-host
+write-host ------------------------------------------------ -foreground Red
+write-host
+write-host Adding ESXi hosts to $vCenter -foreground Yellow
+write-host
+write-host ------------------------------------------------ -foreground Red
+write-host
 
 # Add ESXi hosts
 foreach ($esxihost in $esxihosts) {
-Add-VMHost $esxihost -Location $vcenterlocation -User $esxihostuser -Password $esxihostpasswd
-Add-VMHost $esxihost -User $esxihostuser -Password $esxihostpasswd
+Add-VMHost -name $esxihost -Location $vcenterlocation -User $esxihostuser -Password $esxihostpasswd -force
+#Add-VMHost -name $esxihost -User $esxihostuser -Password $esxihostpasswd
 }
 
 # Disconnect from vCenter Server
-write-host "Disconnecting vCenter Server $vcenter" -foreground green
+write-host
+write-host "Disconnecting from $vcenter" -foreground Green
+write-host
+
 disconnect-viserver -confirm:$false | out-null
+write-host
+Stop-Transcript
